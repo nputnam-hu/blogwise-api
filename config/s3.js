200cf2371f0b55c42d57960fa0738bc1aad6f490
Noah Putnam <noahputnam@gmail.com>
2019-02-05 14:53:45 -0500

    add signatureVersion to s3 init

--- config/s3.js
+++ config/s3.js
@@ -2,6 +2,11 @@ const config = require('./index')
 const AWS = require('aws-sdk')
 
 const { accessKeyId, secretAccessKey, region } = config.S3
-const s3 = new AWS.S3({ accessKeyId, secretAccessKey, region })
+const s3 = new AWS.S3({
+  accessKeyId,
+  secretAccessKey,
+  region,
+  signatureVersion: 'v4',
+})
 
 module.exports = s3
