a9cb4e3dff86be503eabaa738bee8ede6594cd58
Noah Putnam <noahputnam@gmail.com>
2019-02-13 19:35:05 -0500

    push console

--- controllers/netlifyIdentity.js
+++ controllers/netlifyIdentity.js
@@ -6,7 +6,7 @@ const jwt = require('jwt-simple')
 async function handlePassword(email, password, res, next) {
   try {
     const user = await User.findOne({ email })
-    console.log('USER, user')
+    console.log('USER, user', user)
     if (!user) {
       return res
         .status(400)
